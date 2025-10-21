66ade93d124f33d291dfb8620c41ed586440d503
Azamat <azamat201760@yandex.ru>
2025-10-21 15:05:21 +0300

    [add] in undo commnad

---
+++ usecase/command/undo.py
@@ -0,0 +1,24 @@
+from entity.context import CommandContext
+from usecase.interface import HistoryRepository
+
+
+class UndoCmd:
+    def __init__(self, hs: HistoryRepository) -> None:
+        self._hs = hs
+
+    @property
+    def name(self) -> str:
+        """Имя команды"""
+        return 'undo'
+
+    @property
+    def description(self) -> str:
+        """Описание команды"""
+        return 'Отменяет последнюю команду'
+
+    def execute(self, args: list[str], flags: list[str], ctx: CommandContext) -> str:
+        """Выполнение команды, выбрасывает DomainError при ошибке"""
+        last = self._hs.pop()
+        if last:
+            last.undo()
+        return ''
