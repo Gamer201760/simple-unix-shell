4a560a5e4e645c5ad145253925ed288196363612
Azamat <azamat201760@yandex.ru>
2025-10-26 20:17:49 +0300

    [fix] shell add in history repo with -h flag

--- usecase/shell.py
+++ usecase/shell.py
@@ -30,10 +30,10 @@ class Shell:
         if not cmd:
             raise CommandNotFoundError(f'Команда {name} не найдена')
         if '-h' in flags:
-            self._history_repo.add(name, args, flags)  # TODO: refactor
-            return cmd.description
-        res = cmd.execute(args, flags, self._context)
+            res = cmd.description
+        else:
+            res = cmd.execute(args, flags, self._context)
+            if isinstance(cmd, UndoCommand):
+                self._undo_repo.add(cmd.undo())
         self._history_repo.add(name, args, flags)
-        if isinstance(cmd, UndoCommand):
-            self._undo_repo.add(cmd.undo())
         return res
