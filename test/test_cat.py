b48862eb1861fa281064a5a9a48ce5cf8d10d6b3
Azamat <azamat201760@yandex.ru>
2025-10-31 11:19:57 +0300

    [add] tests for cp cat

--- test/test_cat.py
+++ test/test_cat.py
@@ -1,86 +1,55 @@
-from copy import deepcopy
-
 import pytest
 
 from entity.context import CommandContext
 from entity.errors import DomainError, ValidationError
-from repository.in_memory_fs import InMemoryFileSystemRepository
-from usecase.command.cat import Cat
-from usecase.interface import FileSystemRepository
-
-UNIX_TREE = {
-    '/': ['home', 'etc', 'photos'],
-    '/home': ['test', 'test2'],
-    '/home/test': ['etc'],
-    '/home/test/etc': [],
-    '/home/test2': [],
-    '/etc': [],
-    '/photos': ['photo1.png', 'my.png', 'Azamat.jpg'],
-}
-
-
-@pytest.fixture
-def fs(ctx: CommandContext) -> FileSystemRepository:
-    return InMemoryFileSystemRepository(deepcopy(UNIX_TREE))
-
-
-@pytest.fixture
-def cat(fs: FileSystemRepository) -> Cat:
-    return Cat(fs)
+from repository.command.cat import Cat
 
 
 def test_cat_single_file_returns_with_trailing_newline(
-    cat: Cat, fs: FileSystemRepository, ctx: CommandContext
+    cat: Cat, fs, ctx: CommandContext
 ):
-    fs.write('/etc/hosts', 'LINE1')
+    fs.create_dir('/etc')
+    fs.create_file('/etc/hosts', contents='LINE1')
     out = cat.execute(['/etc/hosts'], [], ctx)
     assert out == 'LINE1\n'
 
 
-def test_cat_multiple_files_concatenates_in_order(
-    cat: Cat, fs: FileSystemRepository, ctx: CommandContext
-):
-    fs.write('/etc/hosts', 'H1')
-    fs.write('/home/test/etc/conf', 'C2')
+def test_cat_multiple_files_concatenates_in_order(cat: Cat, fs, ctx: CommandContext):
+    fs.create_dir('/home/test/etc')
+    fs.create_file('/etc/hosts', contents='H1')
+    fs.create_file('/home/test/etc/conf', contents='C2')
     out = cat.execute(['/etc/hosts', '/home/test/etc/conf'], [], ctx)
     assert out == 'H1\nC2\n'
 
 
-def test_cat_keeps_argument_order(
-    cat: Cat, fs: FileSystemRepository, ctx: CommandContext
-):
-    fs.write('/etc/a', 'A')
-    fs.write('/etc/b', 'B')
+def test_cat_keeps_argument_order(cat: Cat, fs, ctx: CommandContext):
+    fs.create_dir('/etc')
+    fs.create_file('/etc/a', contents='A')
+    fs.create_file('/etc/b', contents='B')
     out = cat.execute(['/etc/b', '/etc/a'], [], ctx)
     assert out == 'B\nA\n'
 
 
-def test_cat_error_when_no_args(
-    cat: Cat, fs: FileSystemRepository, ctx: CommandContext
-):
+def test_cat_error_when_no_args(cat: Cat, fs, ctx: CommandContext):
     with pytest.raises(ValidationError):
         cat.execute([], [], ctx)
 
 
-def test_cat_error_when_path_is_directory(
-    cat: Cat, fs: FileSystemRepository, ctx: CommandContext
-):
-    # '/photos' — каталог в исходном дереве
+def test_cat_error_when_path_is_directory(cat: Cat, fs, ctx: CommandContext):
+    fs.create_dir('/photos')
     with pytest.raises(DomainError):
         cat.execute(['/photos'], [], ctx)
 
 
-def test_cat_error_when_file_missing(
-    cat: Cat, fs: FileSystemRepository, ctx: CommandContext
-):
+def test_cat_error_when_file_missing(cat: Cat, fs, ctx: CommandContext):
     with pytest.raises(DomainError):
         cat.execute(['/no/such/file'], [], ctx)
 
 
-def test_cat_raises_on_second_arg_after_first_ok(
-    cat: Cat, fs: FileSystemRepository, ctx: CommandContext
-):
-    fs.write('/etc/one', 'ONE')
+def test_cat_raises_on_second_arg_after_first_ok(cat: Cat, fs, ctx: CommandContext):
+    fs.create_dir('/etc')
+    fs.create_file('/etc/one', contents='ONE')
+    fs.create_dir('/photos')
     with pytest.raises(DomainError):
         cat.execute(['/etc/one', '/photos'], [], ctx)
 
--- test/test_cp.py
+++ test/test_cp.py
@@ -1,104 +1,77 @@
-from copy import deepcopy
+from pathlib import Path
 
 import pytest
 
 from entity.command import Command
 from entity.context import CommandContext
 from entity.errors import ValidationError
-from repository.in_memory_fs import InMemoryFileSystemRepository
-from usecase.command.cp import Cp
-from usecase.interface import FileSystemRepository
-
-UNIX_TREE = {
-    '/': ['home', 'etc', 'photos'],
-    '/home': ['test', 'test2'],
-    '/home/test': ['etc'],
-    '/home/test/etc': [],
-    '/home/test2': [],
-    '/etc': [],
-    '/photos': ['photo1.png', 'my.png', 'Azamat.jpg'],
-}
 
 
-@pytest.fixture
-def ctx() -> CommandContext:
-    return CommandContext(pwd='/home/test', home='/home/test', user='test')
-
-
-@pytest.fixture
-def fs(ctx: CommandContext) -> FileSystemRepository:
-    return InMemoryFileSystemRepository(deepcopy(UNIX_TREE))
-
-
-@pytest.fixture
-def cp(fs: FileSystemRepository) -> Command:
-    return Cp(fs)
-
-
-def test_cp_r_dir_to_new_path_creates_root(
-    cp: Command, fs: FileSystemRepository, ctx: CommandContext
-):
-    assert not fs.is_dir('/backup')
-    fs.write('/photos/photo1.png', 'SRC1')
-    fs.write('/photos/my.png', 'SRC2')
-    fs.write('/photos/Azamat.jpg', 'SRC3')
+def test_cp_r_dir_to_new_path_creates_root(cp: Command, fs, ctx: CommandContext):
+    fs.create_dir('/photos')
+    fs.create_file('/photos/photo1.png', contents='SRC1')
+    fs.create_file('/photos/my.png', contents='SRC2')
+    fs.create_file('/photos/Azamat.jpg', contents='SRC3')
 
     cp.execute(['/photos', '/backup'], ['-r'], ctx)
-    assert fs.is_dir('/backup')
-    assert fs.is_file('/backup/photo1.png')
-    assert fs.read('/backup/photo1.png') == 'SRC1'
-    assert fs.is_file('/backup/my.png')
-    assert fs.read('/backup/my.png') == 'SRC2'
-    assert fs.is_file('/backup/Azamat.jpg')
-    assert fs.read('/backup/Azamat.jpg') == 'SRC3'
+
+    assert Path('/backup').is_dir()
+    assert Path('/backup/photo1.png').is_file()
+    assert Path('/backup/photo1.png').read_text() == 'SRC1'
+    assert Path('/backup/my.png').is_file()
+    assert Path('/backup/my.png').read_text() == 'SRC2'
+    assert Path('/backup/Azamat.jpg').is_file()
+    assert Path('/backup/Azamat.jpg').read_text() == 'SRC3'
 
 
-def test_cp_r_dir_into_existing_dir_places_inside(
-    cp: Command, fs: FileSystemRepository, ctx: CommandContext
-):
+def test_cp_r_dir_into_existing_dir_places_inside(cp: Command, fs, ctx: CommandContext):
+    fs.create_dir('/photos')
+    fs.create_file('/photos/photo1.png', contents='SRC1')
+    fs.create_file('/photos/my.png', contents='SRC2')
+    fs.create_file('/photos/Azamat.jpg', contents='SRC3')
+
+    fs.create_dir('/home')
+
     cp.execute(['/photos', '/home'], ['-r'], ctx)
-    assert fs.is_dir('/home/photos')
-    assert fs.is_file('/home/photos/photo1.png')
-    assert fs.is_file('/home/photos/my.png')
-    assert fs.is_file('/home/photos/Azamat.jpg')
+
+    assert Path('/home/photos').is_dir()
+    assert Path('/home/photos/photo1.png').is_file()
+    assert Path('/home/photos/my.png').is_file()
+    assert Path('/home/photos/Azamat.jpg').is_file()
 
 
-def test_cp_dir_without_r_is_error(
-    cp: Command, fs: FileSystemRepository, ctx: CommandContext
-):
+def test_cp_dir_without_r_is_error(cp: Command, fs, ctx: CommandContext):
+    fs.create_dir('/photos')
     with pytest.raises(ValidationError):
         cp.execute(['/photos', '/home/new_photos'], [], ctx)
 
 
-def test_cp_r_dir_over_file_is_error(
-    cp: Command, fs: FileSystemRepository, ctx: CommandContext
-):
-    fs.write('/photos/my.png', 'OLD_FILE')
+def test_cp_r_dir_over_file_is_error(cp: Command, fs, ctx: CommandContext):
+    fs.create_dir('/photos')
+    fs.create_file('/photos/my.png', contents='OLD_FILE')
+
     with pytest.raises(ValidationError):
-        cp.execute(
-            ['/home'],
-            ['-r'],
-            ctx,
-        )
+        cp.execute(['/home'], ['-r'], ctx)
+
+    fs.create_dir('/home')
     with pytest.raises(ValidationError):
         cp.execute(['/home', '/photos/my.png'], ['-r'], ctx)
 
 
-def test_cp_r_overwrite_with_undo_records(
-    cp: Command, fs: FileSystemRepository, ctx: CommandContext
-):
-    fs.write('/photos/photo1.png', 'SRC1')
-    fs.write('/photos/my.png', 'SRC2')
+def test_cp_r_overwrite_with_undo_records(cp: Command, fs, ctx: CommandContext):
+    fs.create_dir('/photos')
+    fs.create_file('/photos/photo1.png', contents='SRC1')
+    fs.create_file('/photos/my.png', contents='SRC2')
+    fs.create_file('/photos/Azamat.jpg', contents='SRC3')
 
-    if not fs.is_dir('/backup'):
-        fs.mkdir('/backup')
-    if not fs.is_dir('/backup/photos/'):
-        fs.mkdir('/backup/photos')
-    fs.write('/backup/photos/photo1.png', 'OLD1')
+    fs.create_dir('/backup')
+    fs.create_dir('/backup/photos')
+    fs.create_file('/backup/photos/photo1.png', contents='OLD1')
 
     cp.execute(['/photos', '/backup'], ['-r'], ctx)
-    assert fs.read('/backup/photos/photo1.png') == 'SRC1'
-    assert fs.read('/backup/photos/my.png') == 'SRC2'
+
+    assert Path('/backup/photos/photo1.png').read_text() == 'SRC1'
+    assert Path('/backup/photos/my.png').read_text() == 'SRC2'
 
     undo = getattr(cp, 'undo')()
     copied_targets = {u.dst for u in undo}
@@ -111,36 +84,30 @@ def test_cp_r_overwrite_with_undo_records(
         assert x and x.overwrite is True and x.overwritten_path
 
 
-def test_cp_r_conflict_file_vs_dir_in_tree(
-    cp: Command, fs: FileSystemRepository, ctx: CommandContext
-):
-    if not fs.is_dir('/src'):
-        fs.mkdir('/src')
-    if not fs.is_dir('/src/docs'):
-        fs.mkdir('/src/docs')
-    fs.write('/src/docs/readme', 'TEXT')
+def test_cp_r_conflict_file_vs_dir_in_tree(cp: Command, fs, ctx: CommandContext):
+    fs.create_dir('/src')
+    fs.create_dir('/src/docs')
+    fs.create_file('/src/docs/readme', contents='TEXT')
 
-    if not fs.is_dir('/dst'):
-        fs.mkdir('/dst')
-    if not fs.is_dir('/dst/docs'):
-        fs.mkdir('/dst/docs')
-    if not fs.is_dir('/dst/docs/readme'):
-        fs.mkdir('/dst/docs/readme')
+    fs.create_dir('/dst')
+    fs.create_dir('/dst/docs')
+    fs.create_dir('/dst/docs/readme')
 
     with pytest.raises(ValidationError):
         cp.execute(['/src/*', '/dst'], ['-r'], ctx)
 
 
-def test_cp_r_multiple_sources_into_existing_dir(
-    cp: Command, fs: FileSystemRepository, ctx: CommandContext
-):
-    if not fs.is_dir('/mnt'):
-        fs.mkdir('/mnt')
+def test_cp_r_multiple_sources_into_existing_dir(cp: Command, fs, ctx: CommandContext):
+    fs.create_dir('/etc')
+    fs.create_dir('/photos')
+    fs.create_file('/photos/photo1.png', contents='IMG')
+
+    fs.create_dir('/mnt')
 
     cp.execute(['/etc', '/photos', '/mnt'], ['-r'], ctx)
-    assert fs.is_dir('/mnt/etc')
-    assert fs.is_dir('/mnt/photos')
-    assert fs.is_file('/mnt/photos/photo1.png')
+    assert Path('/mnt/etc').is_dir()
+    assert Path('/mnt/photos').is_dir()
+    assert Path('/mnt/photos/photo1.png').is_file()
 
     with pytest.raises(ValidationError):
         cp.execute(['/etc', '/photos', '/mnt/new_place'], ['-r'], ctx)
